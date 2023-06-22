86359edc69a07a6210ee198731a64813fb0b518d
Amitsen11 <89931804+Amitsen11@users.noreply.github.com>
2023-06-22 18:36:48 +0600

    done github public

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const addClassCollection = client.db("ShutterbugDb").collection("addClass");
         const selectedClassesCollection = client.db("ShutterbugDb").collection("selectedClasses");
@@ -404,7 +404,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
